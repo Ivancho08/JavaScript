a909d78b35f3cd1cd383225912b90c5b542c2150
Ivancho08 <ivansantamaria812@gmail.com>
2023-12-13 13:59:40 -0300

    SE ARMA UNA FUNCIÓN Y UN BUCLE PARA PODER CORROBORAR SI UNA CONTRASEÑA ES CORRECTA O NO

---
+++ main.js
@@ -0,0 +1,28 @@
+function passwordVerification(password) {
+  if (password.length >= 8) {
+    if (
+      /[A-Z]/.test(password) &&
+      /[a-z]/.test(password) &&
+      /[0-9]/.test(password)
+    ) {
+      alert("La contraseña ingresada es una contraseña válida");
+      return true;
+    } else {
+      alert(
+        "La contraseña debe tener una mayúsucula, una minúscula y un número por lo menos"
+      );
+      return false;
+    }
+  } else {
+    alert("La contraseña no contiene los carácteres suficientes");
+    return false;
+  }
+}
+
+let password;
+let check = false;
+
+do {
+  password = prompt("Ingrese una contraseña para validar: ");
+  check = passwordVerification(password);
+} while (!password || !check);
