40992c50dcb013cc53779d774cb42a8db880fed4
Ivancho08 <ivansantamaria812@gmail.com>
2024-01-25 10:04:48 -0300

    MODIFICACION DEL DOM Y ESCUCHAS DE EVENTOS

--- main.js
+++ main.js
@@ -1,50 +1,46 @@
-function User() {
-  this.correctPasswords = [];
-}
+document.addEventListener("DOMContentLoaded", function () {
+  function User() {
+    this.correctPasswords = [];
+  }
 
-User.prototype.passwordVerification = function (password) {
-  if (password.length >= 8) {
-    if (
-      /[A-Z]/.test(password) &&
-      /[a-z]/.test(password) &&
-      /[0-9]/.test(password)
-    ) {
-      alert("La contraseña ingresada es una contraseña válida");
-      this.correctPasswords.push(password);
-      return true;
+  User.prototype.passwordVerification = function (password) {
+    if (password.length >= 8) {
+      if (
+        /[A-Z]/.test(password) &&
+        /[a-z]/.test(password) &&
+        /[0-9]/.test(password)
+      ) {
+        this.correctPasswords.push(password);
+        return true;
+      } else {
+        return false;
+      }
     } else {
-      alert(
-        "La contraseña debe tener una mayúsucula, una minúscula y un número por lo menos"
-      );
       return false;
     }
-  } else {
-    alert("La contraseña no contiene los carácteres suficientes");
-    return false;
+  };
+
+  User.prototype.searchPassword = function (password) {
+    return this.correctPasswords.includes(password);
+  };
+
+  let user = new User();
+
+  function passwordVerification() {
+    let passwordInput = document.getElementById("passwordInput");
+    let resultContainer = document.getElementById("resultContainer");
+
+    let password = passwordInput.value;
+    let isValid = user.passwordVerification(password);
+
+    if (isValid) {
+      resultContainer.innerHTML = "Contraseña Valida. Guardada correctamente. ";
+    } else {
+      resultContainer.innerHTML =
+        "La contraseña no cumple con los requisitos necesarios. ";
+    }
   }
-};
 
-User.prototype.searchPassword = function (password) {
-  return this.correctPasswords.includes(password);
-};
-
-let user = new User();
-
-let password;
-let check = false;
-
-do {
-  password = prompt("Ingrese una contraseña para validar: ");
-  check = user.passwordVerification(password);
-} while (!password || !check);
-
-console.log(user.correctPasswords); //Este punto solo lo utilizo para ver si se guardo correctamente la contraseña
-
-let searchPassword = prompt("Ingrese una contraseña a buscar: ");
-let found = user.searchPassword(searchPassword);
-
-if (found) {
-  alert("La contraseña fue encontrada.");
-} else {
-  alert("La contraseña no existe.");
-}
+  let buttonVerification = document.getElementById("buttonVerification");
+  buttonVerification.addEventListener("click", passwordVerification);
+});
